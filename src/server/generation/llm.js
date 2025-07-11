b64e737331f196d8f757f28ae20e669188b9ff0f
Kurac-pluse <ktys.pc@gmail.com>
2025-07-11 16:44:27 +0900

    単純なllmの実装

--- src/server/generation/llm.js
+++ src/server/generation/llm.js
@@ -1,3 +1,33 @@
+// 推論を行う関数
 export async function makeResponse(currentSpeaker) {
-    console.log(currentSpeaker);
+    const url = process.env.REACT_APP_LLAMA_SERVER_URL;
+
+    const body = {
+        prompt: 'Hello I am ' + currentSpeaker,
+        max_tokens: 256,
+        temperature: 0.7,
+        top_p: 0.9,
+    };
+
+    try {
+        console.log(process.env.REACT_APP_LLAMA_SERVER_URL);
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        });
+
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
+
+        const data = await res.json();
+
+        console.log(data.content);
+    } catch (e) {
+      console.error("Error calling LLM API:", e);
+      return "";
+    }
 }
